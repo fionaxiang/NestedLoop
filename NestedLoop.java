96dfb53b08962b26f5b566288e060e2b6c1daec6
unknown <fionaliu8000@gamil.com>
2015-03-26 21:02:27 -0400

    Draw a diamond by using while loop

---
+++ NestedLoop.java
@@ -0,0 +1,45 @@
+
+public class NestedLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Line=1,Line1=1;
+		   while(Line<=1){
+			   System.out.println("\t   "+ "*");
+			  Line++;
+		   }
+		   while(Line<=2){
+			   System.out.println( "\t  "+"***");
+			   Line++;
+		   }while(Line<=3){
+			   System.out.println("\t "+"*"
+			   		+ "**** ");
+			   Line++;
+		   }while(Line<=4){
+			   System.out.println("\t"+"*******");
+			   Line++;
+		   }while(Line1<=1){
+			   System.out.println("\t " + "*****");
+			   Line1++;
+		   }while(Line1<=2){
+			   System.out.println("\t  " + "***");
+			   Line1++;
+		   }while(Line1<=3){
+			   System.out.println("\t   " + "*");
+			   Line1++;
+		   }
+		   
+
+			}
+
+		
+
+		
+	  }
+	
+
+
+
+	
+
+
